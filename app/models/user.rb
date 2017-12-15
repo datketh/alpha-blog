5783fdfa2bef1511bd9e03da292c46568b3e519b
datketh <datketh@gmail.com>
2017-12-15 08:40:55 -0500

    Associate users to articles

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,13 @@
 class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  has_many :articles
+
+  before_save {self.email = email.downcase}
+
   validates :username, presence: true, uniqueness: {case_sensitive: false},
-                                       length: {minimum: 6, maximum: 24}
+                                       length: {minimum: 5, maximum: 24}
   validates :email, presence: true, length: {minimum: 6, maximum: 48},
                                     uniqueness: {case_sensitive: false},
                                     format: {with: VALID_EMAIL_REGEX}
+
 end
