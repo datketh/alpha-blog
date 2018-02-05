3c4ae328c06af60c3f87e899300b6efd81c7510e
datketh <datketh@gmail.com>
2018-02-05 12:18:43 -0500

    Edit users and DRY code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,42 @@
 class UsersController < ApplicationController
   def index
   end
+
   def show
     @user = User.find(params[:id])
   end
+
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Sign up successful!"
+      redirect_to user_path(@user)
+    else
+      render 'new'
+    end
+
   end
   def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = "Profile updated successfully"
+      redirect_to user_path(@user)
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
   end
 end
